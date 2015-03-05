2048d6933a80fd8ddc9f39411ada25bc35b5380c
CMGMB <mbookspan@carbonmedia.com>
2015-03-05 11:44:55 -0500

    Made create_db_users script atomic for each MySQL statement - create users was breaking if failed

--- support/metadata.rb
+++ support/metadata.rb
@@ -1,20 +1,10 @@
 recipe "cmg_oo::vhost_indianasportsman_com", "Sets up the www.indianasportsman.com vhost(s) and alias(es) (if applicable)."
-
 recipe "cmg_oo::vhost_redirect_hoosierhunting_net_to_www_indianasportsman_com", "Sets up the www.indianasportsman.com vhost(s) and alias(es) (if applicable)."
-
 recipe "cmg_oo::vhost_redirect_indianahuntingforum_com_to_www_indianasportsman_com", "Sets up the www.indianasportsman.com vhost(s) and alias(es) (if applicable)."
-
 recipe "cmg_oo::vhost_redirect_indianamasteranglers_com_to_www_indianasportsman_com", "Sets up the www.indianasportsman.com vhost(s) and alias(es) (if applicable)."
-
 recipe "cmg_oo::vhost_redirect_indianaoutdoorsman_com_to_www_indianasportsman_com", "Sets up the www.indianasportsman.com vhost(s) and alias(es) (if applicable)."
-
 recipe "cmg_oo::vhost_mainehuntingforums_com", "Sets up the www.mainehuntingforums.com vhost(s) and alias(es) (if applicable)."
-
 recipe "cmg_oo::vhost_arizonahuntingforums_com", "Sets up the www.arizonahuntingforums.com vhost(s) and alias(es) (if applicable)."
-
 recipe "cmg_oo::vhost_wisconsinoutdoorsman_com", "Sets up the www.wisconsinoutdoorsman.com vhost(s) and alias(es) (if applicable)."
-
 recipe "cmg_oo::vhost_theboxotruth_com", "Sets up the www.theboxotruth.com vhost(s) and alias(es) (if applicable)."
-
 recipe "cmg_oo::vhost_xenforo-addons_carbonmedia_net", "Sets up the www.xenforo-addons_carbonmedia.net vhost(s) and alias(es) (if applicable)."
-
