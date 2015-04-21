9a4a3a09762286b6fea0902d4bb06ab6cb71a294
Rick Christy <rchristy@outdoorhub.com>
2015-04-20 23:35:22 -0400

    Fixed remove_newrelic recipe

--- Cookbooks/cmg_oo/recipes/remove_newrelic.rb
+++ Cookbooks/cmg_oo/recipes/remove_newrelic.rb
@@ -5,13 +5,14 @@
 
 rightscale_marker :begin
 
-service "newrelic-sysmond" do
-  :action stop
+service ["newrelic-sysmond", "newrelic-daemon"] do
+  action :stop
 end
 
 bash "remove_newrelic" do
   code <<-EOF
   	yum -y remove newrelic-sysmond
+    yum -y remove newrelic-daemon
   EOF
 end
 
