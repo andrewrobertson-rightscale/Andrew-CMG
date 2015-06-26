54041d0a9eb831c119d3c4beb876cb21d0e559e9
Rick Christy <rchristy@outdoorhub.com>
2015-06-26 14:49:05 -0400

    Updated new relic recipes

--- Cookbooks/cmg_oo/recipes/remove_newrelic.rb
+++ Cookbooks/cmg_oo/recipes/remove_newrelic.rb
@@ -1,9 +1,44 @@
 #
-# Cookbook Name:: cmg_oo
+# Cookbook Name:: cmg_core
 # Recipe:: remove_newrelic
 #
 
 rightscale_marker :begin
+
+nr_host = "#{node['newrelic']['server_monitoring']['deployment_prefix']} #{node['newrelic']['server_monitoring']['hostname']}"
+my_host = node['hostname']
+
+file "/root/jq" do
+    action :delete
+end
+
+bash "get_jq" do
+    cwd "/root"
+    code <<-EOH
+        wget http://devops.carbonmedia.net/jq
+    EOH
+end
+
+file "/root/jq" do
+    mode '0755'
+    action :create
+end
+
+bash "delete_server" do
+    cwd "/root"
+    code <<-EOH
+        nr_server_id=$( \
+            curl \
+            -X GET 'https://api.newrelic.com/v2/servers.json' \
+            -H 'X-Api-Key:71eceae3fcf2bfc4321bdc9881d65a0b4f9c92052a3334e' \
+            -d "filter[host]=#{my_host}" \
+            | /root/jq .servers[0].id \
+        )
+        echo #{my_host} - #{nr_host}
+        curl -X DELETE "https://api.newrelic.com/v2/servers/$nr_server_id.json" \
+             -H 'X-Api-Key:71eceae3fcf2bfc4321bdc9881d65a0b4f9c92052a3334e'
+    EOH
+end
 
 service ["newrelic-sysmond", "newrelic-daemon"] do
   action :stop
@@ -11,7 +46,7 @@ end
 
 bash "remove_newrelic" do
   code <<-EOF
-  	yum -y remove newrelic-sysmond
+    yum -y remove newrelic-sysmond
     yum -y remove newrelic-daemon
   EOF
 end
--- Cookbooks/cmg_oo/recipes/set_newrelic_hostname.rb
+++ Cookbooks/cmg_oo/recipes/set_newrelic_hostname.rb
@@ -1,20 +1,50 @@
 #
-# Cookbook Name:: cmg_oo
+# Cookbook Name:: cmg_core
 # Recipe:: set_newrelic_hostname
 #
 
 rightscale_marker :begin
 
 nr_host = "#{node['newrelic']['server_monitoring']['deployment_prefix']} #{node['newrelic']['server_monitoring']['hostname']}"
+my_host = node['hostname']
 
-bash "set_newrelic_hostname" do
-  code <<-EOF
-  	echo 'hostname="#{nr_host}"' >> /etc/newrelic/nrsysmond.cfg
-  EOF
+file "/root/jq" do
+    action :delete
 end
 
-service "newrelic-sysmond" do
-  action :restart
+bash "get_jq" do
+    cwd "/root"
+    code <<-EOH
+        wget http://devops.carbonmedia.net/jq
+    EOH
+end
+
+file "/root/jq" do
+    mode '0755'
+    action :create
+end
+
+bash "set_server_name" do
+    cwd "/root"
+    code <<-EOH
+        nr_server_id=$( \
+            curl \
+            -X GET 'https://api.newrelic.com/v2/servers.json' \
+            -H 'X-Api-Key:71eceae3fcf2bfc4321bdc9881d65a0b4f9c92052a3334e' \
+            -d "filter[host]=#{my_host}" \
+            | /root/jq .servers[0].id \
+        )
+        echo #{my_host} - #{nr_host}
+        curl -X PUT "https://api.newrelic.com/v2/servers/$nr_server_id.json" \
+             -H 'X-Api-Key:71eceae3fcf2bfc4321bdc9881d65a0b4f9c92052a3334e' \
+             -H 'Content-Type: application/json' \
+             -d \
+        '{
+          "server": {
+            "name": "#{nr_host}"
+          }
+        }' 
+    EOH
 end
 
 rightscale_marker :end
