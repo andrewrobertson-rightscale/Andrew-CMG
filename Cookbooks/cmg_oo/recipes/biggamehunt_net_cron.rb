3c2eceeb98ee38cd6951f3699c3d517705557281
Rick Christy <rchristy@outdoorhub.com>
2015-12-07 16:36:10 -0500

    Added composer and drush installation pre-requisites.
    https://trello.com/c/XI68Jj9o/2303-biggamehunt-net-issues-per-jim

--- Cookbooks/cmg_oo/recipes/biggamehunt_net_cron.rb
+++ Cookbooks/cmg_oo/recipes/biggamehunt_net_cron.rb
@@ -5,6 +5,31 @@
 
 rightscale_marker :begin
 
+bash "install_composer" do 
+  user "root"
+  cwd "/root"
+  code <<-EOH
+    cd
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
+    ln -s /usr/local/bin/composer /usr/bin/composer
+  EOH
+end
+
+bash "install_drush" do
+  user "root"
+  cwd "/root"
+  code <<-EOH
+    cd
+    git clone https://github.com/drush-ops/drush.git /usr/local/src/drush
+    cd /usr/local/src/drush
+    git checkout 7.0.0-alpha5  #or whatever version you want.
+    ln -s /usr/local/src/drush/drush /usr/bin/drush
+    composer install
+    drush --version    
+  EOH
+end
+
 cron "biggamehunt_net_cron" do
   minute "00"
   command "cd /home/vhosts/biggamehunt.net && /usr/bin/drush cron"
