259c5a218f760ab2cbe6509445a881329f69276b
Rick Christy <rick@rickchristy.com>
2016-03-11 13:08:23 -0500

    Added 360tuna recipes

---
+++ Cookbooks/cmg_oo/recipes/vhost_360tuna_com.rb
@@ -0,0 +1,29 @@
+rightscale_marker :begin
+
+directory "/home/vhosts/360tuna.com" do
+    owner "rightscale"
+    group "apache"
+    mode 00755
+    recursive true
+    action :create
+end
+
+template "/etc/httpd/sites-available/33-www.360tuna.com.conf" do
+    source "vhost_360tuna_com.conf.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    action :create
+end
+
+link "/etc/httpd/sites-enabled/33-www.360tuna.com.conf" do
+    to "/etc/httpd/sites-available/33-www.360tuna.com.conf"
+end
+
+bash "restart_httpd" do
+    code <<-EOF
+        service httpd restart
+    EOF
+end
+
+rightscale_marker :end
