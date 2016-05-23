e4319990e8c9bff872ba32c8c1f7a75216032f28
Rick Christy <rchristy@carbonmedia.com>
2016-05-23 16:53:20 -0400

    Fixed fix_curl_tls frozen repo issue by resolving RPM dependencies

--- Cookbooks/cmg_oo/recipes/fix_curl_tls.rb
+++ Cookbooks/cmg_oo/recipes/fix_curl_tls.rb
@@ -8,6 +8,11 @@ rightscale_marker :begin
 bash "fix_curl_tls" do
 	user "root"
 	code <<-EOF
+		wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+		rpm -Uvh epel-release-latest-6.noarch.rpm
+		rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/Redhat/6/x86_64/ius-release-1.0-10.ius.el6.noarch.rpm
+		yum install -y yum-plugin-replace
+		yum replace -y openssl --replace-with openssl10
 		rpm -Uvh http://www.city-fan.org/ftp/contrib/yum-repo/city-fan.org-release-1-13.rhel6.noarch.rpm
 		yum -y --enablerepo=city-fan.org update curl
 		service httpd restart
