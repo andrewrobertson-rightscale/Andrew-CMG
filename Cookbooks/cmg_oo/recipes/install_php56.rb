958b8fafd425d83abdc5d762e8783a197a018984
Rick Christy <rchristy@carbonmedia.com>
2016-06-22 16:19:08 -0400

    Fixed additional repo dependencies for install_php5

--- Cookbooks/cmg_oo/recipes/install_php56.rb
+++ Cookbooks/cmg_oo/recipes/install_php56.rb
@@ -7,6 +7,9 @@ bash "install_php56" do
   user "root"
   cwd "/root"
   code <<-EOH
+	wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+	rpm -Uvh epel-release-latest-6.noarch.rpm  
+	rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/Redhat/6/x86_64/ius-release-1.0-10.ius.el6.noarch.rpm
     yum -y install yum-plugin-replace
 	yum -y replace php53u --replace-with=php56u
   EOH
