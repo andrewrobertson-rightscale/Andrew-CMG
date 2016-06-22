e4725c453e748bbcddddd5427ddbb7eb2c02b86f
Rick Christy <rchristy@carbonmedia.com>
2016-06-22 16:51:52 -0400

    Fixed orphaned new_relic node key

--- Cookbooks/cmg_oo/recipes/php56_ini.rb
+++ Cookbooks/cmg_oo/recipes/php56_ini.rb
@@ -11,15 +11,14 @@ template "/etc/php.ini" do
   group "root"
   mode "0644"
   variables( 
-            :include_path => ".:/usr/share/pear:/usr/share/php:/var/lib/www/cake-latest/lib:/var/www/html/api.carbonmediagroup.com/rest/server/lib",
-            :upload_max_filesize => "384M",
-            :max_input_time => "7200",
-            :memory_limit => "384M",
-            :post_max_size => "384M",
-            :error_reporting => "E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_WARNING",
-            :short_open_tag => "On",
-            :new_relic_license_key => node['new_relic']['install']['license_key']
-             )
+	:include_path => ".:/usr/share/pear:/usr/share/php:/var/lib/www/cake-latest/lib:/var/www/html/api.carbonmediagroup.com/rest/server/lib",
+	:upload_max_filesize => "384M",
+	:max_input_time => "7200",
+	:memory_limit => "384M",
+	:post_max_size => "384M",
+	:error_reporting => "E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_WARNING",
+	:short_open_tag => "On"
+  )
   action :create
 end
 
