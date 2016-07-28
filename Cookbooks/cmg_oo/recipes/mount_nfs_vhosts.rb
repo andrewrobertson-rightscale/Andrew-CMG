ca8661311b1a7b8afe0fd1c5ef8b4beb7bc8efe2
Richard Shade <rshade@rightscale.com>
2016-07-28 13:03:20 -0500

    mount efs

--- Cookbooks/cmg_oo/recipes/mount_nfs_vhosts.rb
+++ Cookbooks/cmg_oo/recipes/mount_nfs_vhosts.rb
@@ -23,4 +23,17 @@ mount "/home/vhosts" do
   ]
 end
 
+directory "/mnt/efs" do
+  owner "root"
+  group "root"
+  mode "0777"
+  recursive true
+  action :create
+end
+
+execute "mount efs" do
+  command "mount -t nfs4 -o nfsvers=4.1 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-9515d2dc.efs.us-east-1.amazonaws.com:/ /mnt/efs"
+  action :run
+end
+
 rightscale_marker :end
