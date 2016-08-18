8e69a9ca8d69599a36833577e74618732d0d900f
Richard Shade <rshade@rightscale.com>
2016-08-18 08:24:09 -0500

    resetting back to home

--- Cookbooks/cmg_oo/recipes/mount_nfs_vhosts.rb
+++ Cookbooks/cmg_oo/recipes/mount_nfs_vhosts.rb
@@ -13,15 +13,15 @@ directory "/home/vhosts_backup" do
   action :create
 end
 
-mount "/home/vhosts_backup" do
-  device "ec2-54-88-8-232.compute-1.amazonaws.com:/CMGSites/CMGVolume/vhosts"
-  fstype "nfs"
-  options [
-  	"nfsvers=3",
-  	"tcp",
-  	"nolock"
-  ]
-end
+#mount "/home/vhosts_backup" do
+#  device "ec2-54-88-8-232.compute-1.amazonaws.com:/CMGSites/CMGVolume/vhosts"
+#  fstype "nfs"
+#  options [
+#  	"nfsvers=3",
+#  	"tcp",
+#  	"nolock"
+#  ]
+#end
 
 directory "/mnt/efs" do
   owner "root"
