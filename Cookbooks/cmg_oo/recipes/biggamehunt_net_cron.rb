d4cb71068eeb46a5943b0c957bd347b1e2267f2c
Richard Shade <rshade@rightscale.com>
2016-08-19 09:57:51 -0500

    setting to latest 7x version

--- Cookbooks/cmg_oo/recipes/biggamehunt_net_cron.rb
+++ Cookbooks/cmg_oo/recipes/biggamehunt_net_cron.rb
@@ -23,7 +23,7 @@ bash "install_drush" do
     cd
     git clone https://github.com/drush-ops/drush.git /usr/local/src/drush
     cd /usr/local/src/drush
-    git checkout 7.0.0-alpha5  #or whatever version you want.
+    git checkout 7.3.0 #or whatever version you want.
     ln -s /usr/local/src/drush/drush /usr/bin/drush
     composer install
     drush --version    
